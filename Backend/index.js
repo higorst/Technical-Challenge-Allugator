6d88583073adb7dff6601741acfd5cce8b8828fe
Samuel Rebouças de Jesus <samuelreboucas07@hotmail.com>
2020-11-13 20:18:16 -0300

    Creating backend and frontend application base
    Creation of the application base to be used during the development of the technical challenge.

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('config');
+const routes = require('./routes');
+
+const PORT = config.get('server.port');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', routes);
+
+// handle errors
+app.use((err, req, res, next) => {
+    if(err.status === 404){
+        res.status(404).json({message: 'Resource not found.'})
+    } else {
+        res.status(500).json({message: 'Error processing submitted data.'})
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`)
+});
